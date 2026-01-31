f81883b4a5f57344428790c706db7e0684fff002
enrico-antonini1 <enry1997@me.com>
2026-01-31 13:40:21 +0100

    feat: middleware handleError

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from "express"
 // import connection from "./database/dbConnection.js" 
+import handleError from "./middlewares/handleError.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+
+app.get("/", (req, es) => {
+    res.send("Rotta di prova")
+})
+
+app.use(handleError)
 
 app.listen(port, () => {
     console.log(`Server in ascolto alla porta ${port}`)
---
+++ middlewares/handleError.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+
+export default function handleError(err, req, res, next) {
+const time = new Date();
+const environment = process.env.environment
+fs.writeFileSync("logs/log.txt", `${time}: ${err}`);
+
+  res.status(500);
+  return res.json({
+    error: environment === "development" ? err.toString(): "SERVER ERROR",
+    message: "Errore interno del server",
+  });
+}
