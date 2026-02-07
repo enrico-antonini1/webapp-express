e2a0a38ad0ef7fbe1911414af8b83b155c11587f
enrico-antonini1 <enry1997@me.com>
2026-02-07 10:28:11 +0100

    feat: implementazione index e show

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -1,15 +1,67 @@
-function index (req, res) {
-    res.json("prova index")
+import connection from "../database/dbConnection.js";
+
+function index(req, res, next) {
+  // prova
+  // res.json("prova index")
+
+  // implementazione
+  const query = "SELECT * FROM  movies";
+
+  connection.query(query, (err, results) => {
+    if (err) return next(err);
+    // test:
+    // console.log(results)
+    res.json({
+      results,
+    });
+  });
 }
 
+function show(req, res, next) {
+  // prova
+  // return res.json({message: "show"});
 
-function show (req, res) {
-   return res.json({
-        message: "show"
-    });
+  // implementazione
+  const { id } = req.params;
+
+  // 1° query: query film
+
+  const filmQuery = "SELECT * FROM  movies WHERE id = ?";
+
+  connection.query(filmQuery, [id], (err, results) => {
+    if(err) return next(err);
+    // test:
+    // console.log(results)
+
+    if(results.length === 0) {
+        res.status(404);
+        return res.json({
+            error: "NOT FOUND",
+            message: "Film non trovato",
+        });
+    }
+
+    const film = results[0];
+
+    // return res.json(film)
+
+    // 2° query: query recensioni
+    const reviewsQuery = "SELECT * FROM  movies WHERE id = ?";
+
+    connection.query(reviewsQuery, [id], (err, results) => {
+        if(err) return next(err);
+
+        const respObj = {
+            ...film,
+            reviews: reviewsQuery
+        };
+
+        return res.json(respObj)
+    })
+})
 }
 
 export default {
-    index,
-    show,
-};
+  index,
+  show,
+}
